27384194318593e8c2111cbad4982dec797fab40
colgatto <colgatto@protonmail.com>
2017-11-21 17:09:04 +0100

    add config.js

---
+++ config.js
@@ -0,0 +1,54 @@
+var nullPercentage=5;
+var decimalNumber=2;
+
+var charName=[
+	"David",
+	"Marco",
+	"Giulia",
+	"Giuseppe",
+	"Mario",
+	"Luigi",
+	"Susanna"
+];
+var charSurname=[
+	"DeAngelis",
+	"Rossi",
+	"Cruciani",
+	"Bianchi",
+	"Bellan",
+	"Bragato",
+	"Parenzo"
+];
+var charCity=[
+	"Pisa",
+	"Torino",
+	"Milano",
+	"Roma",
+	"Napoli",
+	"Palermo",
+	"Nizza"
+];
+var charSigle=[
+	"AL",
+	"TO",
+	"MI",
+	"RO",
+	"AO",
+	"CU",
+	"EB",
+	"AS",
+	"GE",
+	"VE"
+];
+var charGeneral=[
+	"casa",
+	"cane",
+	"pane",
+	"luce",
+	"more",
+	"fratello",
+	"sorella",
+	"muro",
+	"stereo",
+	"usb"
+];
